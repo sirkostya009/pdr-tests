9dd592289c600350de029d80280cb4a82ab850b5
sirkostya009 <kosta.tovstik@gmail.com>
2026-06-23 12:58:11 +0300

    move config to vite plugin and remove .vscode

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,17 @@
+import vercelAdapter from "@sveltejs/adapter-vercel";
 import { sveltekit } from "@sveltejs/kit/vite";
+import { vitePreprocess } from "@sveltejs/vite-plugin-svelte";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()],
+	plugins: [
+		sveltekit({
+			// https://svelte.dev/docs/kit/integrations
+			preprocess: vitePreprocess(),
+			adapter: vercelAdapter(),
+			prerender: {
+				handleHttpError: "warn",
+			},
+		}),
+	],
 });
